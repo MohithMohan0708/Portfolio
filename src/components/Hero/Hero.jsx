956dbcdbfcaf65fc8da90d582890f9acd3fccdd0
MohithMohan0708 <mohithmohan0708@gmail.com>
2025-04-13 19:19:17 +0530

    feat: added navbar functionality

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className='hero' id='home'>
       <img src={profile_img} alt="" />
       <h1><span>I'm Mohith Mohan Kumar Kancharla,</span> a passionate full stack developer based in India</h1>
       <p> I am passionate about solving problems, proficient in web development, and eager to expand my knowledge in machine learning.</p>
       <div className="hero-action">
-        <div className="hero-connect">Connect With me</div>
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With me</AnchorLink></div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
